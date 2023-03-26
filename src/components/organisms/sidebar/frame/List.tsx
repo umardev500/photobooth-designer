109a3e8d3618694a29bdec995096297c3bcfb78d
umar <umar.dev500@gmail.com>
2023-03-26 18:05:36 +0700

    add font

---
+++ src/components/organisms/sidebar/frame/List.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FixedSizeList } from 'react-window'
+
+interface Props {
+    containerHeight: number
+    images: string[]
+    onClick: (i: number) => void
+}
+
+export const List = React.memo(({ containerHeight, images, onClick }: Props) => {
+    if (containerHeight > 0) {
+        console.log('render child')
+
+        const handleClick = () => {
+            // toast.loading('Loading...')
+            console.log('clicked')
+            // context.setOverlayToggle(true)
+        }
+
+        const Row = () => (
+            <div className="grid grid-cols-2 gap-4 pr-2.5">
+                {images.map((val, i) => (
+                    <img
+                        key={i}
+                        onClick={() => {
+                            onClick(i)
+                        }}
+                        data-type="main"
+                        className="border border-gray-600 h-40 w-full object-cover rounded-sm"
+                        draggable
+                        src={val}
+                        alt="image-list"
+                    />
+                ))}
+            </div>
+        )
+
+        return (
+            <FixedSizeList height={containerHeight} itemCount={1} itemSize={35} width={'100%'}>
+                {Row}
+            </FixedSizeList>
+        )
+    }
+
+    return null
+})
